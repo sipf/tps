d74f0150b1f94301f8b621f0a0fef5536c1610e5
Mathieu Magnin <mathieu.magnin@gmail.com>
2018-04-25 16:50:26 +0200

    Enable mailtrap during switching time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,22 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :mailjet
+
+  # Remove when switch to CC is done
+  if ENV["ENABLE_MAILTRAP"]
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :user_name => '1f164bd814cfb6',
+      :password => '1b37f7e20d17f8',
+      :address => 'smtp.mailtrap.io',
+      :domain => 'smtp.mailtrap.io',
+      :port => '2525',
+      :authentication => :cram_md5
+    }
+  else
+    config.action_mailer.delivery_method = :mailjet
+  end
+
   config.action_mailer.default_url_options = { protocol: 'https', host: ENV["DS_HOST"] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
